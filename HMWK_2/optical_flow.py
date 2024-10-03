a49653a8200783b9ba00b7422d79fe671a31ecc4
luisferro2 <a01378248@itesm.mx>
2024-10-03 14:34:05 -0600

    debugging optical flow not drawing the line

--- HMWK_2/optical_flow.py
+++ HMWK_2/optical_flow.py
@@ -1,6 +1,12 @@
 import cv2 
 import numpy as np
 
+def debug(thing, title):
+    print('----------------------------------------------------')
+    print(title)
+    print(thing )
+    print('----------------------------------------------------')
+
 def lucas_kanade_method(video_path):
     # Read the video 
     cap = cv2.VideoCapture(video_path)
@@ -18,21 +24,42 @@ def lucas_kanade_method(video_path):
     # Create random colors
     color = np.random.randint(0, 255, (100, 3))
  
-    # Take first frame and find corners in it
-    ret, old_frame = cap.read()
+    # Reached first frame.
+    reached_ff = False
+    while not reached_ff:
+        # Take first frame and find corners in it
+        ret, old_frame = cap.read()
+        if np.sum(old_frame) != 0:
+            reached_ff = True    
+
+
     old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
+    #debug(old_gray_int, 'actual first frame')
+
+    #old_gray = old_gray_int / 255
+    #old_gray = old_gray.astype('float32')
+    #debug(old_gray.dtype, 'old gray')
+
     p0 = cv2.goodFeaturesToTrack(old_gray, mask=None, **feature_params)
- 
+    #debug(p0, 'p0')
+
     # Create a mask image for drawing purposes
     mask = np.zeros_like(old_frame)
+    #mask = mask.astype('float32')
+    debug(mask.dtype, 'mask')
 
     while True:
         # Read new frame
         ret, frame = cap.read()
+        #frame = frame_int / 255
+        #frame = frame.astype('float32')
+
         if not ret:
             break
         frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
+        #debug(frame_gray.dtype, 'frame gray')
+
+
         # Calculate Optical Flow
         p1, st, err = cv2.calcOpticalFlowPyrLK(
             old_gray, frame_gray, p0, None, **lk_params
@@ -40,13 +67,23 @@ def lucas_kanade_method(video_path):
         # Select good points
         good_new = p1[st == 1]
         good_old = p0[st == 1]
+        #debug(p1, 'p1')
     
         # Draw the tracks
         for i, (new, old) in enumerate(zip(good_new, good_old)):
             a, b = new.ravel()
             c, d = old.ravel()
-            mask = cv2.line(mask, (a, b), (c, d), color[i].tolist(), 2)
-            frame = cv2.circle(frame, (a, b), 5, color[i].tolist(), -1)
+            debug((int(a), int(b)), 'a b')
+            debug((int(c), int(d)), 'c d')
+            # debug(color[i].tolist(), 'color')
+            cv2.imshow('before line', mask)
+            mask = cv2.line(mask, (int(a), int(b)), (int(c), int(d)), color[i].tolist(), 2)
+            debug(mask.shape, 'mask shape')
+            debug(mask[int(a), int(b)], 'line in mask')
+            debug(mask[int(c), int(d)], 'line in mask')
+            cv2.imshow('after line', mask)
+            #debug(mask, 'mask with line I guess')
+            frame = cv2.circle(frame, (int(a), int(b)), 5, tuple(color[i].tolist()), 2)
     
         # Display the demo
         img = cv2.add(frame, mask)
@@ -59,4 +96,4 @@ def lucas_kanade_method(video_path):
         old_gray = frame_gray.copy()
         p0 = good_new.reshape(-1, 1, 2)
 
-lucas_kanade_method(0)
+lucas_kanade_method('testOpticalFlow.mp4')
